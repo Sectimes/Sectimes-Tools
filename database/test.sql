a51177e0252bbff0d617ee67a94dec0ddd4b3a4c
Sectimes <sectimescyber@gmail.com>
2023-11-08 22:48:12 +0700

    Update Dockerfile and example table for mysql db

---
+++ database/test.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE 'sectimes';
+
+CREATE TABLE `users` (
+  `id` int(20) NOT NULL COMMENT 'id',
+  `fname` varchar(50) NOT NULL COMMENT 'first name'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
